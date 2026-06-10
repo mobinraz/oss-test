9bee5ff8471e8f6f0d5cfd0e3469e59a1813b6af
mobin <mobin.razghandi1375@gmail.com>
2026-06-10 12:48:02 +0330

    fix bugs

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,4 +1,3 @@
-
 import { Mail, Phone, MapPin } from "lucide-react";
 import Image from "next/image";
 
--- components/scroll-top/ScrollTop.tsx
+++ components/scroll-top/ScrollTop.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { ArrowUp } from "lucide-react";
 import { useState, useEffect } from "react";
 
